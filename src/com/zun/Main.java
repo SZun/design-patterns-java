dc9fe145d02a6e416a4ff3831acbf75690c34cad
Zun <zunsamuel97@gmail.com>
2020-08-25 05:41:39 -0500

    impl

--- src/com/zun/Main.java
+++ src/com/zun/Main.java
@@ -3,6 +3,14 @@ package com.zun;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ShapeCache.loadCache();
+
+        Shape clonedShape = (Shape) ShapeCache.getShape("1");
+        System.out.println(clonedShape.getTipo());
+        clonedShape = (Shape) ShapeCache.getShape("2");
+        System.out.println(clonedShape.getTipo());
+        clonedShape = (Shape) ShapeCache.getShape("3");
+        System.out.println(clonedShape.getTipo());
+
     }
 }
---
+++ src/com/zun/ShapeCache.java
@@ -0,0 +1,26 @@
+package com.zun;
+
+import java.util.Hashtable;
+
+public class ShapeCache {
+
+    private static Hashtable<String, Shape> shapeHashtable = new Hashtable<>();
+
+    public static Shape getShape(String shapeId) {
+        return (Shape) shapeHashtable.get(shapeId).clone();
+    }
+
+    public static void loadCache(){
+        Circle circle = new Circle();
+        circle.setId("1");
+        shapeHashtable.put(circle.getId(), circle);
+
+        Square square = new Square();
+        square.setId("2");
+        shapeHashtable.put(square.getId(), square);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setId("3");
+        shapeHashtable.put(rectangle.getId(), rectangle);
+    }
+}
