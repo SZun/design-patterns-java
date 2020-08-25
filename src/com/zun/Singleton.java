c5f576b12407bd4afb80b3b7b208b8821d93f671
Zun <zunsamuel97@gmail.com>
2020-08-25 03:56:53 -0500

    eager eval

--- src/com/zun/Singleton.java
+++ src/com/zun/Singleton.java
@@ -2,15 +2,12 @@ package com.zun;
 
 public class Singleton {
 
-    private volatile static Singleton uniqueInstance = null;
+    private volatile static Singleton uniqueInstance = new Singleton();
     private int data = 0;
 
     private Singleton(){}
 
     public static Singleton getInstance(){
-        if(uniqueInstance == null) synchronized (Singleton.class) {
-            if(uniqueInstance == null) uniqueInstance = new Singleton();
-        }
         return uniqueInstance;
     }
 
