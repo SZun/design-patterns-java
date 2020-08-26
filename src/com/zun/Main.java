e0eb14445642e2eb421f65165d47eca68012af3f
Zun <zunsamuel97@gmail.com>
2020-08-25 23:48:15 -0500

    chal

--- src/com/zun/Main.java
+++ src/com/zun/Main.java
@@ -1,20 +1,12 @@
 package com.zun;
 
 public class Main {
+    public static void main(String [] args)
+    {
+        HotelKeeper keeper = new HotelKeeper();
 
-    public static void main(String[] args) {
-        Amplifier amp = new Amplifier("Top-O-Line Amplifier");
-        Tuner tuner = new Tuner("Top-O-Line AM/FM Tuner", amp);
-        DvdPlayer dvd = new DvdPlayer("Top-O-Line DVD Player", amp);
-        CdPlayer cd = new CdPlayer("Top-O-Line CD Player", amp);
-        Projector projector = new Projector("Top-O-Line Projector", dvd);
-        TheaterLights lights = new TheaterLights("Theater Ceiling Lights");
-        Screen screen = new Screen("Theater Screen");
-        PopcornPopper popper = new PopcornPopper("Popcorn Popper");
-
-        HomeTheaterFacade homeTheater = new HomeTheaterFacade(amp, tuner, dvd, cd,
-                projector, screen, lights, popper);
-        homeTheater.watchMovie("Inception");
-        homeTheater.endMovie();
+        VegMenu v = keeper.getVegMenu();
+        NonVegMenu nv = keeper.getNonVegMenu();
+        Both b = keeper.getVegNonMenu();
     }
 }
