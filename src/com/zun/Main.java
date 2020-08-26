0778e48f73a10ec3011c2185fef227bea727707e
Zun <zunsamuel97@gmail.com>
2020-08-25 23:39:34 -0500

    impl

--- src/com/zun/Main.java
+++ src/com/zun/Main.java
@@ -3,6 +3,18 @@ package com.zun;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Amplifier amp = new Amplifier("Top-O-Line Amplifier");
+        Tuner tuner = new Tuner("Top-O-Line AM/FM Tuner", amp);
+        DvdPlayer dvd = new DvdPlayer("Top-O-Line DVD Player", amp);
+        CdPlayer cd = new CdPlayer("Top-O-Line CD Player", amp);
+        Projector projector = new Projector("Top-O-Line Projector", dvd);
+        TheaterLights lights = new TheaterLights("Theater Ceiling Lights");
+        Screen screen = new Screen("Theater Screen");
+        PopcornPopper popper = new PopcornPopper("Popcorn Popper");
+
+        HomeTheaterFacade homeTheater = new HomeTheaterFacade(amp, tuner, dvd, cd,
+                projector, screen, lights, popper);
+        homeTheater.watchMovie("Inception");
+        homeTheater.endMovie();
     }
 }
