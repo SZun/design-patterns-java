96927742ffdc2d74b4a67974583a33fea126a222
Zun <zunsamuel97@gmail.com>
2020-08-25 21:08:08 -0500

    impl

--- src/com/zun/Main.java
+++ src/com/zun/Main.java
@@ -3,6 +3,9 @@ package com.zun;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Vehicle vehicle = new Car(new Produce(), new Assemble());
+	    vehicle.manufacture();
+	    vehicle = new Bike(new Produce(), new Assemble());
+	    vehicle.manufacture();
     }
 }
---
+++ src/com/zun/Workshop.java
@@ -0,0 +1,23 @@
+package com.zun;
+
+abstract class Workshop {
+
+    abstract public void work();
+
+}
+
+class Produce extends Workshop {
+
+    @Override
+    public void work() {
+        System.out.println("Produced");
+    }
+}
+
+class Assemble extends Workshop {
+
+    @Override
+    public void work() {
+        System.out.println("And Assembled");
+    }
+}
