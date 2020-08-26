6d66084ba8f850120d1d23a2fb2d945611ed51b5
Zun <zunsamuel97@gmail.com>
2020-08-26 05:08:06 -0500

    impl

---
+++ src/com/zun/Currency.java
@@ -0,0 +1,13 @@
+package com.zun;
+
+public class Currency {
+    private int amount;
+
+    public Currency(int amt) {
+        amount = amt;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
--- src/com/zun/Main.java
+++ src/com/zun/Main.java
@@ -1,8 +1,40 @@
 package com.zun;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main {
+    private DispenseChain c1;
+
+    public Main() {
+        // initialize the chain
+        this.c1 = new Dollar50Dispensor();
+        DispenseChain c2 = new Dollar20Dispenser();
+        DispenseChain c3 = new Dollar10Dispenser();
+
+        // set the chain of responsibility
+        c1.setNextChain(c2);
+        c2.setNextChain(c3);
+    }
+
+    public static void main(String[] args)
+    {
+        Main atmDispensor = new Main();
+
+        while(true)
+        {
+            int amount = 0;
+
+            System.out.println("Enter amount to dispense");
+            Scanner input = new Scanner(System.in);
+            amount = input.nextInt();
+
+            if (amount % 10 != 0) {
+                System.out.println("Amount should be in multiple of 10s.");
+                return;
+            }
+
+            // process the request
+            atmDispensor.c1.dispense(new Currency(amount));
+        }
     }
 }
