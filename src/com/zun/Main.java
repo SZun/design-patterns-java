2071c491b6ac828c4a75b1f2b8209a44d0402bd8
Zun <zunsamuel97@gmail.com>
2020-08-26 23:26:14 -0500

    impl

--- src/com/zun/Main.java
+++ src/com/zun/Main.java
@@ -1,8 +1,19 @@
 package com.zun;
 
+import java.util.Arrays;
+
 public class Main {
 
+    private static final ItemElement[] items = new ItemElement[]{
+            new Book(20, "1234"),new Book(100, "5678"),
+            new Fruit(10, 2, "Banana"), new Fruit(5, 5, "Apple")
+    };
+
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Total Cost = "+ calculatePrice(items));
+    }
+
+    private static int calculatePrice(ItemElement[] items) {
+        return Arrays.stream(items).mapToInt(i -> i.accept(new ShoppingCartVisitorImpl())).sum();
     }
 }
